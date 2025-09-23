3c3850cd4984294004aaf616bcbb6088ff933bbe
The Coder <dev@aqtis.io>
2025-09-23 11:19:06 +0200

    feat: Subgraph information agents

---
+++ src/lib/apollo.ts
@@ -0,0 +1,8 @@
+// /lib/apollo.ts
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { publicEnv } from "./env.public";
+
+export const client = new ApolloClient({
+    link: new HttpLink({ uri: publicEnv.NEXT_PUBLIC_AITA_SUBGRAPH }),
+    cache: new InMemoryCache(),
+});
